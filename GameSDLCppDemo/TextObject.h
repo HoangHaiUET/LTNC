7e6f303eff4d54ec07089b0abb449e7c782982bf
HoangHaiUET <23020660@vnu.edu.vn>
2024-04-12 00:03:05 +0700

    fix

--- GameSDLCppDemo/TextObject.h
+++ GameSDLCppDemo/TextObject.h
@@ -7,22 +7,22 @@
 
 class TextObject : public BaseObject {
 public:
-    enum TextColor {
-        RED_TEXT = 0,
-        WHITE_TEXT = 1,
-        BLACK_TEXT = 2
-    };
+	enum TextColor {
+		RED_TEXT = 0,
+		WHITE_TEXT = 1,
+		BLACK_TEXT = 2
+	};
 
-    TextObject();
-    ~TextObject();
+	TextObject();
+	~TextObject();
 
-    void SetText(const std::string& text) { str_val_ = text; }
-    void SetColor(const int& type);
-    void CreateGameText(TTF_Font* font, SDL_Surface* des);
+	void SetText(const std::string& text) { str_val_ = text; }
+	void SetColor(const int& type);
+	void CreateGameText(TTF_Font* font, SDL_Surface* des);
 
 private:
-    std::string str_val_;
-    SDL_Color text_color_;
+	std::string str_val_;
+	SDL_Color text_color_;
 };
 
 #endif
